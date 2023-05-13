67fa5e7d0023eb7abe7b6c3faff1a7e001c3d513
Boram Jeong <shelter.sune@gmail.com>
2023-05-13 14:15:49 +0900

    update sample app

--- SampleApp/TodoApp/main.py
+++ SampleApp/TodoApp/main.py
@@ -22,34 +22,7 @@ def redirect_index():
 def index():
     return "static/index.html"
 
-@app.get("/connect", response_class=FileResponse)
-def connect():
-    return "static/html/connect.html"
-
 @app.get('/hcheck')
 def hcheck():
     return "OK"
-
-@app.post("/connect_to_db")
-async def connect_to_db(dbConnection: DBConnection):
-    data = json.dumps(dbConnection.dict()).encode('utf-8')
-
-    async with aiofiles.open('static/db_connection.config', 'wb') as out_file:
-        await out_file.write(data)  # async write chunk
-
-    db_connection.make_table()
-
-    return {"Result": "OK"}
-
-@app.post("/todo/add")
-def add_todo(todo: Todo):
-    db_connection.add_todo(todo)
-
-@app.get("/todo/read", response_model=List[Todo])
-def read_todo():
-    return db_connection.read_todo()
-
-@app.post("/todo/update")
-def update_todo(todo: Todo):
-    db_connection.update_todo(todo)
 
