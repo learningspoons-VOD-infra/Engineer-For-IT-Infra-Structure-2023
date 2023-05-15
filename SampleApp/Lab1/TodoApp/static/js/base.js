5e0a14b7197af5dcae52dbca4458f5abb8e311b0
Boram Jeong <shelter.sune@gmail.com>
2023-05-15 16:29:50 +0900

    update sample app

--- SampleApp/Lab1/TodoApp/static/js/base.js
+++ SampleApp/Lab1/TodoApp/static/js/base.js
@@ -1,22 +1,12 @@
 $(document).ready(function () {
-    getTodos()
-    
-    function getTodos(){
-        const url = 'todo/read'
+    $('#btnAdd').click(function () {
+        const todo = {
+            "todo": String($('#txtTodo').val()),
+            "is_done": 0,
+            "created": new Date()
+        }
 
-        $.ajax({
-            url: url,
-            method: "GET",
-            contentType: "application/json",
-        }).done(function (todos) {
-            console.log(todos)
-            loadTodos(todos)
-        });
-    }
-
-    function loadTodos(todos){
-        for (const todo of todos) {
-            todoText = '<span>'+todo.todo+'</span>'
+        todoText = '<span>'+todo.todo+'</span>'
             todoDone = '<input class="form-check-input me-2" type="checkbox" value="" />'
             if(todo.is_done == 1){
                 todoText = '<span><s>'+todo.todo+'</s></span>'
@@ -33,88 +23,5 @@ $(document).ready(function () {
                 <i class="fas fa-times text-primary"></i>\
                 </a>\
             </li>')
-            }
-
-            $('input:checkbox').change(function(){
-                spanEle = $(this).parent().children('span')
-                todoText = $(spanEle).text()
-                checked = $(this).is(":checked")
-                if(checked){
-                    $(spanEle).html('<s>'+todoText+'</s>')
-                } else {
-                    $(spanEle).html(todoText)
-                }
-
-                const url = 'todo/update'
-
-                const data = {
-                    "todo": String(todoText),
-                    "is_done": Number(checked),
-                    "created": new Date()
-                }
-
-                var inputData = JSON.stringify(data)
-
-                $.ajax({
-                    url: url,
-                    method: "POST",
-                    data: inputData,
-                    dataType: "json",
-                    contentType: "application/json",
-                }).done(function (result) {
-                });
-                
-                
-            })
-
-    }
-
-    
-    $('#btnAdd').click(function () {
-        const url = 'todo/add'
-
-        const data = {
-            "todo": String($('#txtTodo').val()),
-            "is_done": 0,
-            "created": new Date()
-        }
-
-        var inputData = JSON.stringify(data)
-
-        $.ajax({
-            url: url,
-            method: "POST",
-            data: inputData,
-            dataType: "json",
-            contentType: "application/json",
-        }).done(function (result) {
-            $('#txtTodo').val('')
-            $('#todoContainer').empty()
-            getTodos()
-        });
-    })
-
-    $('#btnConnect').click(function () {
-        const url = 'connect_to_db'
-
-        const data = {
-            "serverName": String($('#txtServerName').val()),
-            "admin": String($('#txtAdmin').val()),
-            "password": String($('#txtPassword').val()),
-        }
-
-        var inputData = JSON.stringify(data)
-
-        $.ajax({
-            url: url,
-            method: "POST",
-            data: inputData,
-            dataType: "json",
-            contentType: "application/json",
-        }).done(function (result) {
-            $('#result').append('  <div class="alert alert-primary" role="alert">\
-            DB Connection Success!\
-          </div>')
-        });
     })
 });
